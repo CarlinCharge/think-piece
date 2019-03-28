29b55bd0392cba17594324786a2ca2e7c71a6768
Carl Conroy <sprintman210@gmail.com>
2019-03-27 20:41:17 -0400

    adds subscribe and unsubscribe listeners

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -9,35 +9,27 @@ class Application extends Component {
     posts: []
   };
 
-  componentDidMount = async () => {
-    const snapshot = await firestore.collection('posts').get()
-      
-    const posts = snapshot.docs.map(collectIdsAndDocs);
-    
-    this.setState({posts});
+  unsubscribe = null;
 
-  };
+  componentDidMount = async () => {
+   this.unsubscribe = firestore.collection('posts').onSnapshot(snapshot => {
+    const posts = snapshot.docs.map(collectIdsAndDocs);
+    this.setState({ posts});
+   });
+  }
+
+  componentWillUnmount = () => {
+    this.unsubscribe();
+  }
 
   handleCreate = async post => {
-    const { posts } = this.state;
-
-    const docRef = await firestore.collection('posts').add(post);
-    const doc = await docRef.get();
-
-    const newPost = collectIdsAndDocs(doc);
-
-    this.setState({ posts: [newPost, ...posts] });
+ firestore.collection('posts').add(post);
   };
 
   handleRemove = async id => {
-    const allPosts = this.state.posts;
+  firestore.doc(`posts/${id}`).delete();
 
-    await firestore.doc(`posts/${id}`).delete();
-
-    const posts = allPosts.filter(post => post.id !== id);
-
-    this.setState({posts});
-  }
+  };
 
   render() {
     const { posts } = this.state;
